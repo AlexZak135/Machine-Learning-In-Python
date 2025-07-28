d83caed9a24400e75f93a891d939751192760498
AlexZak135 <azakrzeski13@gmail.com>
2025-07-28 08:59:10 -0400

    Update

--- Code/Supervised-Machine-Learning-Code.py
+++ Code/Supervised-Machine-Learning-Code.py
@@ -1,5 +1,13 @@
-# Title: Supervised Machine Learning
+# Title: Supervised Machine Learning Module
 # Author: Alexander Zakrzeski
-# Date: July 27, 2025
+# Date: July 28, 2025
 
-#
+# Load to import, clean, and wrangle data
+import polars as pl
+from sklearn.datasets import load_breast_cancer
+
+# Load to train, test, and evaluate machine learning models
+
+# Part 1: Machine Learning Workflow
+
+bc = pl.from_pandas(load_breast_cancer(as_frame = True).frame)
