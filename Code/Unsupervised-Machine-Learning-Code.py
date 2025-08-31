c1c6510ed5687638a0c1f1c855ee8e5faac46681
AlexZak135 <azakrzeski13@gmail.com>
2025-08-31 19:32:03 -0400

    Update

--- Code/Unsupervised-Machine-Learning-Code.py
+++ Code/Unsupervised-Machine-Learning-Code.py
@@ -96,3 +96,103 @@ def create_clusters(df):
 
 clusters = create_clusters(customers)
 
+customers = pd.read_csv("Mall-Customers-Data.csv")[["Annual Income", 
+                                                    "Spending Score"]]
+
+def get_centroids(df, k):
+    df_sample = df.sample(k, random_state = 123).reset_index(drop = True)
+    df_ll = df_sample.values.tolist()
+    
+    return df_sample, df_ll
+
+centroids, coords = get_centroids(customers, 2)
+
+def calculate_distance(df, centroids_coords):
+    names = []
+    
+    for i, centroid in enumerate(centroids_coords):
+        df[f"dist_centroid_{i + 1}"] = (
+            np.sqrt((df.iloc[:, 0] - centroid[0]) ** 2 + 
+                    (df.iloc[:, 1] - centroid[1]) ** 2)
+            )
+        
+        names.append(f"dist_centroid_{i + 1}")
+    
+    return df, names
+
+customers, dist_names = calculate_distance(customers, coords)
+
+customers["cluster"] = (
+    customers[dist_names].idxmin(axis = 1).str.split("_").str[-1]
+    )
+
+sns.scatterplot(x = "Annual Income", y = "Spending Score", hue = "cluster", 
+                palette = "tab10", data = customers, s = 50)
+sns.scatterplot(x = "Annual Income", y = "Spending Score", color = "black", 
+                data = centroids, s = 100)
+plt.show()
+
+new_centroids = (
+    round(customers.groupby("cluster")[customers.columns[:2]].mean(), 4)
+    )
+
+new_coords = new_centroids.values.tolist()
+
+customers = pd.read_csv("Mall-Customers-Data.csv")
+customers = customers[["Annual Income", "Spending Score"]]
+variables = customers.columns
+centroids, coords = get_centroids(customers, 2)
+
+for i in range(100):
+    last_coords = coords.copy()
+    customers, dist_names = calculate_distance(customers, coords)
+    customers["cluster"] = (
+        customers[dist_names].idxmin(axis = 1).str.split("_").str[-1]
+        )
+    centroids = round(customers.groupby("cluster")[variables].mean(), 4)
+    coords = centroids.values.tolist()
+    
+    if coords == last_coords:
+        break
+
+print(f"Total Iterations: {i + 1}")
+                       
+sns.scatterplot(x = "Annual Income", y = "Spending Score", hue = "cluster", 
+                palette = "tab10", data = customers, s = 50)
+sns.scatterplot(x = "Annual Income", y = "Spending Score", color = "black", 
+                data = centroids.reset_index(drop = True), s = 100)
+plt.show()
+
+customers = pd.read_csv("Mall-Customers-Data.csv")
+customers = customers[["Annual Income", "Spending Score"]]
+
+def kmeans(df, k, n_iterations = 100):
+    variables = df.columns
+    
+    centroids, coords = get_centroids(df, k)
+
+    for i in range(n_iterations):
+        last_coords = coords.copy()
+        df, dists = calculate_distance(df, coords)
+        df["cluster"] = df[dists].idxmin(axis = 1).str.split("_").str[-1]
+        centroids = round(df.groupby("cluster")[variables].mean(), 4)
+        coords = centroids.values.tolist()
+
+        if last_coords == coords:
+      	    break
+    
+    print(f"Total Iterations: {i + 1}")
+
+    fig, ax = plt.subplots(figsize = (10, 5))
+    sns.scatterplot(x = variables[0], y = variables[1], hue = "cluster", 
+                    palette = "tab10", data = df, s = 50, ax = ax)
+    sns.scatterplot(x = variables[0], y = variables[1], color = "black", 
+                    data = centroids, s = 100, ax = ax)
+
+
+    plt.tight_layout()
+    plt.show()
+
+    return df["cluster"]
+
+clusters = kmeans(customers, 2)
