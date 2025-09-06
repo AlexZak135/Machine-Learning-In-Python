99c448c46a1df218e9eadeac68cd781e8f515a07
AlexZak135 <azakrzeski13@gmail.com>
2025-09-05 21:05:08 -0400

    Update

--- Code/Unsupervised-Machine-Learning-Code.py
+++ Code/Unsupervised-Machine-Learning-Code.py
@@ -1,12 +1,14 @@
 # Title: Unsupervised Machine Learning Module 
 # Author: Alexander Zakrzeski
-# Date: September 3, 2025
+# Date: September 5, 2025
 
+################################################################################
 import numpy as np
 import os
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
@@ -64,4 +66,42 @@ clusters = model.fit_predict(customers_scaled)
 customers["Cluster"] = clusters + 1
 print(customers["Cluster"].value_counts())
 
-# 6 of 9
+fig = plt.figure(figsize = (20, 10))
+
+for i, column in enumerate(["Age", "Annual Income", "Spending Score"]):
+    df_plot = customers.groupby("Cluster")[column].mean()
+    ax = fig.add_subplot(2, 2, i + 1)
+    ax.bar(df_plot.index, df_plot, color = sns.color_palette("Set1"), 
+           alpha = 0.6)
+    ax.set_title(f"Average {column.title()} per Cluster", alpha = 0.5)
+    ax.xaxis.grid(False)
+    
+plt.tight_layout()
+plt.show()
+
+fig, axs = plt.subplots(2, 2, figsize = (16, 8))
+sns.scatterplot(x = "Age", y = "Annual Income", hue = "Cluster", 
+                data = customers, palette = "Set1", alpha = 0.4, ax = axs[0][0])
+sns.scatterplot(x = "Age", y = "Spending Score", hue = "Cluster", 
+                data = customers, palette = "Set1", alpha = 0.4, ax = axs[0][1],
+                legend = False)
+sns.scatterplot(x = "Annual Income", y = "Spending Score", hue = "Cluster", 
+                data = customers, palette = "Set1", alpha = 0.4, ax = axs[1][0])
+
+plt.tight_layout()
+plt.show()
+
+plot_df = pd.crosstab(
+    index = customers["Cluster"], columns = customers["Gender"], 
+    values = customers["Gender"], aggfunc = "size", normalize = "index"
+    )
+
+fig, ax = plt.subplots(figsize = (12, 6))
+plot_df.plot.bar(stacked = True, ax = ax, alpha = 0.6)
+ax.set_title(f"% Gender per Cluster", alpha = 0.5)
+ax.set_ylim(0, 1.4)
+ax.legend(frameon = False)
+ax.xaxis.grid(False)
+
+plt.tight_layout()
+plt.show()
