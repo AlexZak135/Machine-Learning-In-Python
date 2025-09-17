67dd3d45b086de3d096c921a95ed3a377bb7598a
AlexZak135 <azakrzeski13@gmail.com>
2025-09-16 21:34:40 -0400

    Update

--- Code/Unsupervised-Machine-Learning-Code.py
+++ Code/Unsupervised-Machine-Learning-Code.py
@@ -1,10 +1,13 @@
 # Title: Unsupervised Machine Learning Module 
 # Author: Alexander Zakrzeski
-# Date: September 15, 2025
+# Date: September 16, 2025
 
 import os
 import polars as pl
 
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 os.chdir("/Users/atz5/Desktop/Machine-Learning-In-Python/Data")
@@ -30,10 +33,46 @@ num_cols = ["age", "dependent_count", "estimated_income", "months_on_book",
             "total_relationship_count", "months_inactive_12_mon", 
             "credit_limit", "total_trans_amount", "total_trans_count", 
             "avg_utilization_ratio"]
-scaled = StandardScaler().fit_transform(customers[num_cols].to_numpy())
+scaled = StandardScaler().fit_transform(customers.select(num_cols))
 customers_scaled = customers.with_columns([
     pl.Series(name, scaled[:, i]) for i, name in enumerate(num_cols)
     ])
+
+def plot_elbow_curve(df, max_clusters):
+    inertias = [] 
+    
+    for k in range(1, max_clusters + 1): 
+        model = KMeans(n_clusters = k, random_state = 123)
+        model.fit(df)
+        inertias.append(round(model.inertia_, 2)) 
+        
+    plt.figure(figsize = (12, 8))
+    plt.plot(range(1, max_clusters + 1), inertias, marker = "o") 
+    plt.xticks(ticks = range(1, max_clusters + 1), 
+               labels = range(1, max_clusters + 1))
+    plt.title("Inertia vs. Number of Clusters")
+    plt.tight_layout()
+    plt.show()
+    
+    return inertias
+
+plot_elbow_curve(customers_scaled.select("age", "estimated_income", 
+                                         "credit_limit", "total_trans_amount", 
+                                         "avg_utilization_ratio"), 10)
+plot_elbow_curve(customers_scaled.select("total_relationship_count", 
+                                         "months_inactive_12_mon", 
+                                         "total_trans_amount", 
+                                         "total_trans_count", 
+                                         "avg_utilization_ratio"), 10)
+plot_elbow_curve(customers_scaled.select("dependent_count", "estimated_income",
+                                         "months_on_book", "credit_limit", 
+                                         "avg_utilization_ratio"), 10)
+
+
+
+
+
+
 
 
 
@@ -54,62 +93,15 @@ total_trans_count: the overall number of times the customer used the card.
 avg_utilization_ratio: daily average utilization ratio.
 ################################################################################
 ################################################################################
-import numpy as np
 
-import pandas as pd
 
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 
-from sklearn.cluster import KMeans
 
 
-
-customers = pd.read_csv("Mall-Customers-Data.csv")
-
-model = KMeans(n_clusters = 5)
-clusters = model.fit_predict(customers[["Annual Income", "Spending Score"]])
-
-def plot_elbow_curve(df, max_clusters = 10):
-    
-    inertias = [] 
-    
-    for k in range(1, max_clusters + 1):
-        model = KMeans(n_clusters = k, random_state = 744)
-        cluster = model.fit_predict(df)
-        inertias.append(round(model.inertia_, 2)) 
-        
-    plt.figure(figsize = (12, 8))
-    plt.plot(range(1, max_clusters + 1), inertias, marker = "o")    
-    plt.xticks(ticks = range(1, max_clusters + 1), 
-               labels = range(1, max_clusters + 1))
-    plt.title("Inertia vs Number of Clusters")
-    plt.tight_layout()
-    plt.show()
-    
-    return inertias
-  
-inertias = plot_elbow_curve(customers[["Annual Income", "Spending Score"]])
-print(inertias)
-
-customers = customers[["Annual Income", "Spending Score"]] 
-scaler = StandardScaler()
-scaler.fit(customers)
-customers = scaler.transform(customers)
-
-inertias = plot_elbow_curve(customers)
-print(inertias)
-
-customers = pd.read_csv("Mall-Customers-Data.csv")
-customers = customers.drop(columns = "CustomerID")
-customers["Gender"] = np.where(customers["Gender"] == "Male", 1, 0)
-
-scaler = StandardScaler()
-scaler.fit(customers)
-customers_scaled = scaler.transform(customers)
 
 inertias = plot_elbow_curve(customers_scaled)
-print(inertias)
 
 model = KMeans(n_clusters = 6, random_state = 123)
 clusters = model.fit_predict(customers_scaled)
